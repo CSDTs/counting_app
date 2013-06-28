3db1f118c50bf4cc3ea80781986dea9a20a5db9f
Michael O'Keefe <okeefm@rpi.edu>
2013-06-28 09:23:59 -0400

    Cleaning something

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -28,22 +28,109 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
+	
+	bluetoothSerial: null,
+	//db: null,
+	
     // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+		//document.addEventListener("offline", app.phoneOffline, false);
+		app.bluetoothSerial = cordova.require('bluetoothSerial');
+		app.bluetoothSerial.isEnabled(app.bluetoothSuccess, function() {
+			navigator.notification.alert("Please enable Bluetooth!");
+		});
+		//app.db = window.openDatabase("sensordata", "1.0", "Sensor Data", 200000);
+		//app.db.transaction(app.sqlSetup, app.errorCB, app.successCB);
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+	
+	sqlSetup: function() {
+		tx.executeSql('DROP TABLE IF EXISTS sensordata');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS sensordata (sensor_id, data, timestamp)');
+	},
+	
+	// Transaction error callback
+    //
+    errorCB: function(tx, err) {
+        alert("Error processing SQL: "+err);
+    },
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    // Transaction success callback
+    //
+    successCB: function() {
+        alert("success!");
+    },
+	
+	phoneOffline: function() {
+		navigator.notification.alert("You have no data connection; this will be a problem for uploading data!");
+		console.log("offline!");
+		document.addEventListener("online", app.phoneOnline, false);
+		document.addEventListener("offline", app.phoneOnline, false);
+	},
+	
+	phoneOnline: function() {
+		navigator.notification.alert("Hey, your phone is online again; excellent!");
+		console.log("online!");
+		document.removeEventListener("online", app.phoneOnline, false);
+		document.addEventListener("offline", app.phoneOffline, false);
+	},
+	
+	bluetoothSuccess: function() {
+		console.log("Bluetooth enabled");
+		if (window.location.pathname.indexOf("upload.html") != -1) {
+			app.bluetoothSerial.list(function(list) {
+				$(list).each(function(i, v) {
+					//console.log(v['name']);
+					$("#bluetoothId").append("<option value='" + v['address'] + "'>" + v['name'] + "</option>");
+				});
+				$("#bluetoothId").removeProp("disabled");
+			});
+			$("#connectBt").click(function(e) {
+				e.preventDefault();
+				app.bluetoothSerial.connect($("#bluetoothId").val(), app.btConnectSuccess, app.btConnectFailure);
+			});
+		}
+	},
+	
+	btConnectSuccess: function() {
+		navigator.notification.alert("Bluetooth connected!");
+		$("#uploadData").removeClass("disabled");
+		app.bluetoothSerial.write("$#");
+		app.bluetoothSerial.subscribe('\n', function(data) {
+			console.log(data);
+			$("#data").val($("#data").val() + data + "\n");
+			$data_split = data.split(",");
+			/*app.db.transaction(function(tx) {tx.executeSql("INSERT INTO sensordata (sensor_id, data, timestamp) VALUES ('" + data['sensor_id'] + "', '" + data['data'] + "', " + data['timestamp'] + ";")}, 
+				app.errorCB, appsuccessCB);*/
+			if ($data_split[1].indexOf("timestamp") == -1 && $data_split[1].length >= 12) {
+				$.post("http://www.communitysensors.rpi.edu/sensor-maps/new_store_data.php", { c_sensor_id: $data_split[0], c_user_temp: $data_split[2], c_date_time: $data_split[1]}, function(data) {
+					console.log(data);
+				});
+			}
+				
+		}, function() {
+			navigator.notification.alert("Bluetooth read error");
+		});
 
-        console.log('Received Event: ' + id);
-    }
+	},
+	
+	uploadButton: function() {
+		app.bluetoothSerial.available(function (numBytes) {
+			console.log("There are " + numBytes + " bytes available to read.");
+			if (numBytes == 0) {
+				alert("All data uploaded!");
+			}
+		}, function () {
+			console.log("available failure");
+		});
+		app.bluetoothSerial.unsubscribe();
+		app.bluetoothSerial.disconnect();
+	},
+	
+	btConnectFailure: function() {
+		navigator.notification.alert("Bluetooth not connected!");
+		$("#uploadData").addClass("disabled");
+		app.bluetoothSerial.unsubscribe();
+	}
 };
+
+app.initialize();
